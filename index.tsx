e0b9fba287cc12ab133c9250aef4d21cb8c3abb6
kimkonan <kimkonan97@gmail.com>
2024-09-07 04:58:04 +0900

    first homework

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as ReactDOMClient from 'react-dom/client';
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { darkTheme, lightTheme } from "./theme";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
